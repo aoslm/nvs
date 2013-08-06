fdf6afe52307a7a53a3f27aad0768015a864662e
aoslm <aoslm12193@gmail.com>
2013-08-06 22:56:40 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nvs::Application.config.secret_key_base = '7cac97cba6d9a8f3d5973af718bdc810af4e4fcbb37564b2aff982eceb376c527573d9a4aab2a93e7a3ee7cb7abb5666491888e1fea27e7d344df66ee6e0ca57'
